0de42a5f2bbee9515c4bc79122ef85febe515f94
Donald Njumbe Ediage <ediage@yahoo.com>
2019-05-01 14:26:39 +0300

    udating database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require("./controlers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "donaldediage",
-    password: "",
-    database: "smart-brain"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
