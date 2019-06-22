f91479bbc50ccd0b0fd86e75d2cd228c09a3aaf4
Donald Njumbe Ediage <ediage@yahoo.com>
2019-06-22 11:46:47 +0300

    checking database

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send(db.users);
+  res.send("It is working!");
 });
 app.post("/signin", (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
