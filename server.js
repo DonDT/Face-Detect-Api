f32a5a35493870e6d92f69067ab67a89676dfab8
Donald Njumbe Ediage <ediage@yahoo.com>
2019-06-22 12:57:10 +0300

    fixing database

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const image = require("./controlers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    database: "smart-brain"
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
